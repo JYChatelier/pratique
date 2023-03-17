85a942b03c5b1a779e665c29710f95a4d13cd34b
JYChatelier <jean-yves.chatelier@ineris.fr>
2023-03-17 08:13:38 +0000

    Lectures CSV et Parquet des fichiers Sirene et Naiades

--- gestion_sparklyr.R
+++ gestion_sparklyr.R
@@ -1,19 +1,25 @@
 library(sparklyr)
-library(dplyr)
 
 
 #spark_install(version = "3.3")
 #sc <- spark_connect(master = "local", version = '3.3')
 
+#Ouverture de la session Spark déjà configurée
 sc <- spark_connect(master = "k8s://https://kubernetes.default.svc:443")
 
-mtcars_spark <- copy_to(sc, mtcars, "mtcars")
+#Lecture du fichier Sirene au format CSV
+spark_sirene_csv <- spark_read_csv(sc,
+                                   name = "sirene_csv",
+                                   path = "s3a://projet-spark-lab/diffusion/formation/data/sirene/sirene.csv")
+#Lecture du fichier Sirene au format parquet
+spark_sirene_parquet <- spark_read_parquet(sc,
+                                           name = "sirene_parquet",
+                                           path = "s3a://projet-spark-lab/diffusion/formation/data/sirene.parquet")
 
 
-
-df <- aws.s3::s3read_using(FUN = arrow::read_parquet,
-                            object = "analyse_2009_2021.csv.parquet", 
-                            bucket = "projet-iquale",
-                            opts = list("region" = ""))
-
-arrow::write_parquet(df,"analyse_2009_2021.csv.parquet")
+spark_naiades_csv <- spark_read_csv(sc,
+                                    name = "naiades_csv",
+                                    path = "s3a://projet-iquale/analyse_2009_2021.csv")
+spark_naiades_parquet <- spark_read_parquet(sc,
+                                            name = "naiades_parquet",
+                                            path = "s3a://projet-iquale/analyse_2009_2021.csv.parquet")
